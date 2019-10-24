806e9902e4345963c18b193a26adb28efdcadc04
taylorz <taylor.zanke@gmail.com>
2019-10-24 18:34:59 -0400

    captions layout

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -6,8 +6,9 @@ import Whole from '../../components/Whole/Whole';
 import Fragment from '../../components/Fragment/Fragment';
 
 import TwoCitiesReader1 from '../../assets/images/1a.png';
-import TwoCitiesReader2 from '../../assets/images/5a.png';
-import DrawingsModels1 from '../../assets/images/3b.png'
+import TwoCitiesReader2 from '../../assets/images/1b.png';
+import DrawingsModels1 from '../../assets/images/3b.png';
+import DrawingsModels2 from '../../assets/images/3c.png';
 
 class Homepage extends Component {
   render() {
@@ -17,13 +18,13 @@ class Homepage extends Component {
           <Whole workImage={TwoCitiesReader1} title="Two Cities Diary" year="2014"/>
           <Grid container className="fragments" justify="space-between">
             <Grid item md={4} xs={12} className="fragment-container">
-              <Fragment fragmentImage={TwoCitiesReader2} title="Two Cities Reader" year="2014"/>
-            </Grid>
-            <Grid item md={6} xs={12} className="fragment-container">
               <Fragment fragmentImage={DrawingsModels1} title="Two Cities Reader" year="2014"/>
             </Grid>
+            <Grid item md={6} xs={12} className="fragment-container">
+              <Fragment fragmentImage={DrawingsModels2} title="Two Cities Reader" year="2014"/>
+            </Grid>
           </Grid>
-          <Whole workImage={TwoCitiesReader1}/>
+          <Whole workImage={TwoCitiesReader2}/>
         </PageContainer>
       </>
     );
