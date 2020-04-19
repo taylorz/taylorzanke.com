2e3a737deda3fba4683e31eac0aa3405566001ce
taylorz <taylor.zanke@gmail.com>
2020-04-19 11:30:11 -0700

    carousel progress

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './Hero.scss';
+
+import TestImage from "../../assets/images/works/NightDrawings/NightDrawings3.jpeg"
+import TestImage2 from "../../assets/images/works/NightDrawings/NightDrawings1.jpeg"
+import TestImage3 from "../../assets/images/works/NightDrawings/NightDrawings2.jpeg"
 
 class Hero extends Component {
   render() {
@@ -19,10 +25,39 @@ class Hero extends Component {
       <div className={`hero-container ${className}`}>
         <Grid container className="hero-contents">
           <Grid item xs={12} md={6} lg={5} className="hero-image-section">
+            <Carousel
+              showThumbs={false}
+              showIndicators={false}
+              showArrows={false}
+              showStatus={false}
+              autoPlay={true}
+              interval={3000}
+              slide={false}
+              infiniteLoop={true}
+            >
+                <div>
+                    <img src={TestImage} />
+                    <div className="caption-wrapper">
+                    hij lk;jas dlfkj askljdf; aj
+                    </div>
+                </div>
+                <div>
+                    <img src={TestImage2} />
+                    <div className="caption-wrapper">
+                    2 kjasd ;lfjka
+                    </div>
+                </div>
+                <div>
+                    <img src={TestImage3} />
+                    <div className="caption-wrapper">
+                    3 kjsad ;fjkla
+                    </div>
+                </div>
+            </Carousel>
           </Grid>
           <Grid item xs={12} md={6} className="hero-statement-section">
             <Grid container className="statement-wrapper">
-              <Grid item xs={12} md={12} lg={8} className="statement-copy">Taylor Zanke is a Canadian artist and designer living in Los Angeles, CA. He produces books, models, drawings, and recordings.</Grid>
+              <Grid item xs={12} md={12} lg={10} className="statement-copy">Taylor Zanke is a Canadian artist and designer living in Los Angeles, CA. He produces books, models, drawings, and recordings.</Grid>
             </Grid>
           </Grid>
         </Grid>
