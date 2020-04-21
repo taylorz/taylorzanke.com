d861d53dd27f75d1f74f392dac73bd2ecef7e7ef
taylorz <taylor.zanke@gmail.com>
2020-04-20 21:14:03 -0700

    quotes

--- src/pages/Aboutpage/Aboutpage.js
+++ src/pages/Aboutpage/Aboutpage.js
@@ -28,7 +28,10 @@ class Aboutpage extends Component {
           <PageContainer className="aboutpage">
             <Header onClick={() => this.toggleMenu()} linkName="HOME" link="/"/>
             <Grid container className="about-content">
-              <Grid item xs={12} md={6} lg={5} className="artist-statement">Taylor Zanke is a Canadian artist living in Los Angeles, CA. He produces books, models, drawings, and recordings, in an effort to investigate and reveal new potentials for space.</Grid>
+              <Grid item xs={12} md={6} lg={5} className="artist-statement">
+              <p>Taylor Zanke is an artist and designer living in Los Angeles, CA. He takes a multi-format approach to art making, producing models, drawings, photographs, and recordings, many of which are represented in singular artist books. Taylor’s practice studies the shifting nature of potential in space, and develops long-form methodologies for reflecting on and revealing it. He develops ways in which our inner lives can be supported by the material world around us through grafting personal reflections into common forms and proposals.</p>
+              <p>Taylor holds a BFA from Parsons the New School for Design, and a Masters of Architecture and Masters of Science in Real Estate Development from Columbia University.</p>
+              </Grid>
               <Grid item xs={12} md={5} lg={4} xl={3} className="about-details">
                 <div className="details-section">
                   <div className="section-title">CONTACT</div>
@@ -37,11 +40,6 @@ class Aboutpage extends Component {
                     <li>instagram.com/taylorzanke</li>
                   </ul>
                   <p>Please do reach out to discuss collaborations or commissions.</p>
-                </div>
-                <div className="details-section">
-                  <div className="section-title">ANNOUNCEMENTS</div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla nisi vel pulvinar condimentum. Nulla quis sollicitudin lectus. Quisque vel mauris eu neque aliquet cursus. Vivamus efficitur tortor at metus commodo, id luctus nibh tincidunt. Ut efficitur enim in tincidunt lobortis. Donec ac lobortis erat, at dapibus nunc.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla nisi vel pulvinar condimentum. Nulla quis sollicitudin lectus. Quisque vel mauris eu neque aliquet cursus. Vivamus efficitur tortor at metus commodo, id luctus nibh tincidunt. Ut efficitur enim in tincidunt lobortis. Donec ac lobortis erat, at dapibus nunc.</p>
                 </div>
                 <div className="details-section">
                   <div className="section-title">REFERENCES</div>
