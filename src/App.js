18bd52df59966212e2bcd2d32484436546e8c6c9
taylorz <taylor.zanke@gmail.com>
2020-06-30 14:53:11 -0700

    clean up app js format

--- src/App.js
+++ src/App.js
@@ -9,56 +9,44 @@ import './App.css'
 
 import WORKS from './constants/works/works'
 
-// const routes = [
-//   { path: '/', name: 'Home', Component: Home },
-  // { path: '/about', name: 'About', Component: About },
-  // { path: '/transmissions-from-rare-space', name: 'Transmissions From Rare Space', Component: TransmissionsFrom },
-  // { path: '/direction-zine', name: 'Direction Zine', Component: DirectionZine },
-  // { path: '/several-speculative-improvements', name: 'Several Speculative Improvements to my Family Home', Component: SeveralSpeculative },
-  // { path: '/two-cities-diary-honolulu-newyork', name: 'Two Cities Diary Honolulu New York', Component: TwoCitiesDiaryHNY },
-  // { path: '/two-cities-diary-mumbai-paris', name: 'Two Cities Diary Mumbai Paris', Component: TwoCitiesDiaryMP },
-  // { path: '/reformulations-1', name: 'Reformulations 1', Component: Reformulations1 },
-  // { path: '/two-visitations', name: 'Two Visitations', Component: TwoVisitations },
-// ]
-
 class App extends Component {
   render() {
     return (
       <div className="App" >
       <Router>
-          <ScrollToTop/>
-          <Route render={({location}) => {
-            const { key } = location
-            return (
-              <>
-              <TransitionGroup component={null}>
+        <ScrollToTop/>
+        <Route render={({location}) => {
+          const { key } = location
+          return (
+            <>
+            <TransitionGroup component={null}>
               <CSSTransition
                 key={key}
                 appear={true}
                 classNames="my-node"
                 timeout={{enter: 250, exit: 250}}
               >
-              <Switch location={location}>
-                <Route key={"/"} exact path={"/"}>
-                <div className="my-node">
-                  <Homepage/>
-                  </div>
-                </Route>
-                {WORKS.map(( work, index ) => (
-                  <Route key={index} exact path={work.route}>
-                    {({ match }) => (
-                        <div className="my-node">
-                          <ProjectPage />
-                        </div>
-                    )}
+                <Switch location={location}>
+                  <Route key={"/"} exact path={"/"}>
+                    <div className="my-node">
+                      <Homepage/>
+                    </div>
                   </Route>
-                ))}
-              </Switch>
+                  {WORKS.map(( work, index ) => (
+                    <Route key={index} exact path={work.route}>
+                      {({ match }) => (
+                          <div className="my-node">
+                            <ProjectPage />
+                          </div>
+                      )}
+                    </Route>
+                  ))}
+                </Switch>
               </CSSTransition>
-              </TransitionGroup>
-              </>
-            )
-          }}/>
+            </TransitionGroup>
+            </>
+          )
+        }}/>
         </Router>
       </div>
     )
