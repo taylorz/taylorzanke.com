b1755b8baefa76ed6f6805ec837e2abbee218904
taylorz <taylor.zanke@gmail.com>
2020-06-30 12:00:47 -0700

    small tweaks

--- src/components/WorkImage/WorkImage.js
+++ src/components/WorkImage/WorkImage.js
@@ -4,7 +4,7 @@ import './WorkImage.scss';
 
 const WorkImage = ({ image }) => (
     <Grid container className="work-image">
-        <Grid item container xs={12} alignItems="center" justify="center">
+        <Grid item container className="image-block" xs={12} alignItems="center" justify="center">
             <img src={image}/>
         </Grid>
     </Grid>
