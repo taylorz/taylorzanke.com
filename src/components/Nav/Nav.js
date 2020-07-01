7dc32b160b09e2bd00e7cc09d1c4b264a06992fe
taylorz <taylor.zanke@gmail.com>
2020-06-30 18:18:07 -0700

    nav in progress

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Switch, NavLink} from "react-router-dom"
 import './Nav.scss';
+
+import WORKS from '../../constants/works/works'
 
 const Nav = () => (
   <Grid container className="nav">
     <Grid item xs={12}>
       <div className="home"><Link to="/">Taylor Zanke</Link></div>
+    </Grid>
+    <Grid item xs={12}>
+    <ul>
+        {WORKS
+          .sort(({ id: previousID }, { id: currentID }) => currentID - previousID)
+          .map((work) =>
+          <Link to={work.url} activeClassName="nav-link-active">
+            <li>
+            <Grid container className="nav-link-content">
+              <Grid item xs={9} className="title">{work.name}</Grid>
+            </Grid>
+            </li>
+          </Link>
+        )}
+        </ul>
     </Grid>
   </Grid>
 )
