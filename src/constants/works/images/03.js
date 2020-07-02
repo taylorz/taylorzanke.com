bd1745bda9454e9fac31edef68992545d5ace60c
taylorz <taylor.zanke@gmail.com>
2020-07-01 17:57:42 -0700

    mobile sizing

--- src/constants/works/images/03.js
+++ src/constants/works/images/03.js
@@ -8,19 +8,6 @@ import image03_7 from '../../../assets/works/03/03-7.png'
 import image03_8 from '../../../assets/works/03/03-8.png'
 import image03_9 from '../../../assets/works/03/03-9.png'
 import image03_10 from '../../../assets/works/03/03-10.png'
-import image03_11 from '../../../assets/works/03/03-11.png'
-import image03_12 from '../../../assets/works/03/03-12.png'
-import image03_13 from '../../../assets/works/03/03-13.png'
-import image03_14 from '../../../assets/works/03/03-14.png'
-import image03_15 from '../../../assets/works/03/03-15.png'
-import image03_16 from '../../../assets/works/03/03-16.png'
-import image03_17 from '../../../assets/works/03/03-17.png'
-import image03_18 from '../../../assets/works/03/03-18.png'
-import image03_19 from '../../../assets/works/03/03-19.png'
-import image03_20 from '../../../assets/works/03/03-20.png'
-import image03_21 from '../../../assets/works/03/03-21.png'
-import image03_22 from '../../../assets/works/03/03-22.png'
-import image03_23 from '../../../assets/works/03/03-23.png'
 
 
 const WORK_03 = [
@@ -34,20 +21,7 @@ const WORK_03 = [
     image03_7,
     image03_8,
     image03_9,
-    image03_10,
-    image03_11,
-    image03_12,
-    image03_13,
-    image03_14,
-    image03_15,
-    image03_16,
-    image03_17,
-    image03_18,
-    image03_19,
-    image03_20,
-    image03_21,
-    image03_22,
-    image03_23,
+    image03_10
 ]
 
 export default WORK_03
