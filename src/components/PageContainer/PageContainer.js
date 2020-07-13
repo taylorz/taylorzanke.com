b8a42a939feb520275401635d9feee2594ac91cf
taylorz <taylor.zanke@gmail.com>
2020-07-13 10:43:09 -0700

    layout change

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -6,11 +6,11 @@ import WorkList from '../WorkList/WorkList'
 
 const PageContainer = ({ className, children, center }) => (
   <Grid container spacing={2} className={`page-container ${className}`} justify={`${center ? "center" : "flex-start"}`}>
-    <Grid item xs={12} sm={6}>
+    <Grid item xs={12}>{children}</Grid>
+    <Grid item xs={12}>
       <div className="home"><Link to="/">Taylor Zanke</Link></div>
       <WorkList/>
     </Grid>
-    <Grid item xs={12} sm={6}>{children}</Grid>
   </Grid>
 )
 
