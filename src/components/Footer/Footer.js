f4b76336cc48b2a9587ec1b847a7af8bde5795e1
taylorz <taylor.zanke@gmail.com>
2020-08-08 16:23:16 -0700

    bio update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.scss';
 const Footer = () => (
     <Grid item xs={12} className="footer-container">
         <Grid item xs={12} className="bio">
-            <p>Taylor Zanke is an artist and designer living in Los Angeles, CA. He takes a multi-format approach to art making: producing models, drawings, and photographs, many of which are represented in singular artist books. Taylor’s practice studies the shifting nature of potential, present in familiar space, and develops long-form methodologies for reflecting on and revealing it. He uncovers ways in which our inner lives can be supported by the material realities around us, grafting personal reflections into common forms or proposals.</p>
+            <p>Taylor Zanke is an artist and designer living in Los Angeles, CA. He takes a multi-format approach to art making: producing models, drawings, and photographs, many of which are represented in singular artist books. Taylor’s practice studies the shifting nature of potentials, present in familiar and mundane space, and develops long-form methodologies for reflecting on and revealing it. He uncovers ways in which our inner lives can be supported by the material realities around us, grafting personal reflections into commonplace forms or proposals.</p>
             <p>For more information:<br/>mail@taylorzanke.com</p>
             <p>For collaborations:<br/><a href="https://taylorzankeoffice.com" target="_blank">taylorzankeoffice.com</a></p>
         </Grid>
