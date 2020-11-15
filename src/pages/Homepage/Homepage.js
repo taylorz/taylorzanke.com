c8b4c989b9b418f67022e7956e4fcac45eca60d8
taylorz <taylor.zanke@gmail.com>
2020-11-14 17:12:22 -0800

    small update

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -13,8 +13,9 @@ const Homepage = () => (
       <Grid item xs={6} sm={2}>
         <NavLink exact to="/">TAYLOR ZANKE</NavLink>
       </Grid>
-      <Grid item xs={6} sm={2}><a href="https://www.littleuniverse.com/" target="_blank">LITTLE UNIVERSE</a></Grid>
-      <Grid item xs={6} sm={2}><a href="https://www.taylorzankeoffice.com/" target="_blank">TAYLOR ZANKE, OFFICE</a></Grid>
+      <Grid item xs={6} sm={2}>.COM</Grid>
+      {/* <Grid item xs={6} sm={2}><a href="https://www.littleuniverse.com/" target="_blank">LITTLE UNIVERSE</a></Grid>
+      <Grid item xs={6} sm={2}><a href="https://www.taylorzankeoffice.com/" target="_blank">TAYLOR ZANKE, OFFICE</a></Grid> */}
     </Grid>
     <Grid container spacing={2}>
       <Grid item xs={12} sm={4} className="page-section statement">
@@ -23,6 +24,7 @@ const Homepage = () => (
         <p>In my role at Little Universe I work closely with our clients to find opportunities for design to bring new forms of value to their digital platforms. Sometimes this means developing a product from scratch, in other cases it means identifying interventions which can be made in an existing context to address a specific need. Both of these conditions bring exciting opportunities to write code, develop and critique design, and operate as a member of our team. To me, the key design question of our practice at Little Universe, among many others, is whether the forms we choose are appropriate and relevant within the domain we have situated ourselves: addressing existential threat of climate change. In the context of digital product design this means asking questions about what engineering decisions are made, what user experience patterns are used, and what interactions feel like. It also means forming opinions about our design ethic, and questioning ideas like engagement, efficiency, function, clarity, optimism, hope, and joy. Through Little Universe I try to do my part in fulfilling the climate mandate critical to our future as a species.</p>
         <p>In my self directed projects I develop experimental ways of working and thinking which are personal and habitual. Though formalized as an office the output of my efforts are not intended to be transacted and the processes are not intended to be systematized, but rather they are meant to grow from a certain frame of mind, finding stability in themselves through a process of serendipity and accretion. Artistically I am focused on the form of the mundane and the development of diaristic responses to it. I attempt to transcribe and store perceptions, feelings, or memories I experience with familiar and common forms. These efforts have resulted in a practice of practices, within which I develop books, photographs, models, drawings, and recordings. Often these media influence one another, or make appearances within or alongside each other, as trains-of-thought and inner lives are carried across days, months, or years. In this practice I strive for a thematic density capable of attracting its own development.</p>
         <p>I would be excited to discuss any of the above with you, and would be happy to communicate about collaborations. Thank you for visiting my website.</p>
+        <p>All the best,</p>
         <p>Taylor</p>
       </Grid>
       <Grid item xs={12} sm={2} className="page-section  education">
@@ -32,18 +34,19 @@ const Homepage = () => (
           <li><span>B.F.A.</span> Parsons The New School for Design</li>
         </ul>
       </Grid>
-      {/* <Grid item xs={12} sm={2} className="page-section  links">
-        <p className="header">PLACES</p>
+      <Grid item xs={12} sm={2} className="page-section  links">
+        <p className="header">SITES</p>
         <ul>
           <li className="no-spacing"><a href="https://www.littleuniverse.com/" target="_blank">Little Universe</a></li>
           <li className="no-spacing"><a href="https://www.taylorzankeoffice.com/" target="_blank">Taylor Zanke, Office</a></li>
         </ul>
-      </Grid> */}
+      </Grid>
       <Grid item xs={12} sm={2} className="page-section  links">
         <p className="header">CONTACT</p>
         <ul>
           <li className="no-spacing">mail@taylorzanke.com</li>
           <li className="no-spacing"><a href="https://www.instagram.com/taylorzanke/" target="_blank">Instagram</a></li>
+          <li className="no-spacing"><a href="https://taylorzanke.bandcamp.com/" target="_blank">Bandcamp</a></li>
         </ul>
       </Grid>
     </Grid>
