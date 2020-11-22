a8b61efb30bb226f6521d8f96721e649d3e2c982
Taylor Zanke <taylor.zanke@gmail.com>
2020-11-21 17:18:23 -0800

    not much

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import './Footer.scss';
 const Footer = () => (
     <Grid container className="footer-container">
         <Grid item className="copyright">
+            &copy; 2020
         </Grid>
     </Grid>
 )
