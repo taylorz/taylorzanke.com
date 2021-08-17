a999037772494a7b4614697d2f1113ce920dd99c
Taylor Zanke <taylor.zanke@gmail.com>
2021-08-17 10:58:45 -0400

    update copy

--- src/constants/books/BOOK_004.js
+++ src/constants/books/BOOK_004.js
@@ -6,7 +6,7 @@ const BOOK_004 = {
   id: 4,
   title: 'Transmissions From Rare Space',
   year: '2019',
-  description: 'This book collects gestural plan and reflected ceiling plan drawings of imagined spaces which have been scanned and drawn over in a CAD environment. The individual drawings are made up of layers of graphite and printed ink, the latter tracing the former, creating effects of depth. Line weight and poché, applied in a the digital drawing environemnt contribute to a compositional strengthening of gestural line. A conceptual "found condition" in the graphite line is surveyed and stabilized by the CAD line.',
+  description: 'This book collects gestural plan and reflected ceiling plan drawings of imagined spaces which have been scanned and drawn over in a CAD environment. The individual drawings are made up of layers of graphite and printed ink, the latter tracing the former, creating effects of depth. Line weight and poché applied in a the digital drawing environemnt compositionally strengthen the gestural line. A conceptual "found condition" in the graphite line is surveyed and stabilized with the CAD line.',
   dimensions: '7 x 9 inches',
   pagesCount: 192,
   textStock: 'Red River, 26lb, Colorjet',
