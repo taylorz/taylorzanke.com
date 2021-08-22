42343d6db56bf7a5aef5f265b5a9ca78b1444f62
Taylor Zanke <taylor.zanke@gmail.com>
2021-08-22 13:46:12 -0400

    copy change

--- src/constants/books/BOOK_007.js
+++ src/constants/books/BOOK_007.js
@@ -6,7 +6,7 @@ const BOOK_007 = {
   id: 7,
   title: 'Forms Sites',
   year: '2020',
-  description: 'This book presents details of drawings from a prior project (Transmissions From Rare Space). The book leads with a quote from Aldo Rossi which situates the contents. The book represents an inquiry into a prior process of addition and reflection, and the re-presentation of that process as a new context for discovery. The logical presentation of fragmented, and biographical, additions.',
+  description: 'This book presents details of drawings from a prior project (Transmissions From Rare Space). The book leads with a quote from Aldo Rossi which situates the contents. The book represents an inquiry into a prior process of addition and reflection, and the re-presentation of that process as a new context for discovery. The logical presentation of fragmented, and autobiographical, additions.',
   dimensions: '7 x 9.5 inches',
   pagesCount: 208,
   textStock: 'Red River, 26lb, Colorjet',
