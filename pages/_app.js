74084700c58e44b08a2b517de6c5319c2a120bef
Taylor Zanke <taylor.zanke@gmail.com>
2023-10-31 20:04:03 -0700

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyle, theme } from "../styles/theme";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/amf.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component pageProps={pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
+export default MyApp;
