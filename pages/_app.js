ce6ade3988d43470866d91538a1872f7ba9799d2
Taylor Zanke <taylor.zanke@gmail.com>
2023-11-04 09:02:39 -0700

    reinstall next with tailwind:

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,5 @@
-import Head from "next/head";
-import styled, { ThemeProvider } from "styled-components";
-import { GlobalStyle, theme } from "../styles/theme";
+import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="/amf.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component pageProps={pageProps} />
-      </ThemeProvider>
-    </>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-export default MyApp;
