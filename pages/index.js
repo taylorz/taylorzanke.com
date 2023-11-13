638f711b80da8e22b03381feccb308602f270b3e
Taylor Zanke <taylor.zanke@gmail.com>
2023-11-12 20:45:18 -0800

    build the carousela dna bout page

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,130 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleIncrement = () => {
+    if (currentIndex === slides.length - 1) {
+      setCurrentIndex(0);
+      setIsLoaded(false);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+      setIsLoaded(false);
+    }
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`p-2 font-sans text-base leading-8 w-screen overflow-hidden `}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={`flex flex-col gap-2`}>
+        <div className={`flex grid grid-cols-1 sm:grid-cols-2`}>
+          <div>
+            <p className={`indent-16`}>
+              <Link href="/information">Taylor Zanke</Link>
+            </p>
+          </div>
+          <div>
+            <p>
+              {slides.map((slide, i) => (
+                <span
+                  key={i}
+                  onClick={() => setCurrentIndex(i)}
+                  className={`cursor-pointer`}
+                >
+                  <span className={i === currentIndex ? `underline` : null}>
+                    {i + 1}
+                  </span>
+                  {i === slides.length - 1 ? null : `, `}
+                </span>
+              ))}
+            </p>
+          </div>
+        </div>
+        <div
+          className={`flex justify-center sm:h-[calc(100vh-6rem)]`}
+          onClick={handleIncrement}
+        >
+          <div className={`absolute ${isLoaded ? "opacity-0" : "opacity-1"}`}>
+            <p>Loading image...</p>
+          </div>
+          <Image
+            className={`w-full h-auto sm:w-auto sm:h-full cursor-pointer z-1 ${
+              isLoaded ? "opacity-1" : "opacity-0"
+            }`}
+            src={slides[currentIndex].url}
+            alt="Name of work"
+            width={1}
+            height={1}
+            sizes="100vw"
+            priority
+            onLoad={() => setIsLoaded(true)}
+          />
+        </div>
+        <div className={`flex justify-center`}>
+          <p>
+            <span className={`italic`}>{slides[currentIndex]?.title}</span>.{" "}
+            {slides[currentIndex]?.view}. {slides[currentIndex]?.materials}.{" "}
+            {slides[currentIndex]?.year}.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
+
+const slides = [
+  {
+    url: "/taylor.jpg",
+    title: "The Factual Reality Of The Structure",
+    view: "Detail view",
+    materials: "Various materials",
+    year: "2023",
+  },
+  {
+    url: "/taylor-2.jpg",
+    title: "To Store Everything In Matter",
+    view: "Installation view",
+    materials: "Paper, plastic, string, rubber",
+    year: "2023",
+  },
+  {
+    url: "/taylor-3.jpg",
+    title: "The Suspending Effect",
+    view: "Detail view",
+    materials: "Fabric, wood, cabinets",
+    year: "2023",
+  },
+  {
+    url: "/taylor-4.jpg",
+    title: "The Third Sphere of Spontaneity",
+    view: "Detail view",
+    materials: "Various materials",
+    year: "2023",
+  },
+  {
+    url: "/taylor-5.jpg",
+    title: "To Store Everything In Matter",
+    view: "Installation view",
+    materials: "Various materials",
+    year: "2023",
+  },
+  {
+    url: "/taylor-6.jpg",
+    title: "The Poetic Narrative",
+    view: "Installation view",
+    materials: "Hardware, bulb, electrical cord",
+    year: "2023",
+  },
+  {
+    url: "/taylor-7.jpg",
+    title: "Trending Toward The Unity Of Concerns",
+    view: "Installation view",
+    materials: "Carton, paper",
+    year: "2023",
+  },
+];
---
+++ pages/information.js
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+export default function Information() {
+  return (
+    <main
+      className={`p-2 font-sans text-base leading-8 w-screen overflow-hidden `}
+    >
+      <div className={`grid grid-cols-1 sm:grid-cols-2`}>
+        <div>
+          <p className={`indent-16`}>
+            <Link href="/">Taylor Zanke</Link>
+          </p>
+        </div>
+        <div>
+          <p>
+            Taylor Zanke is an artist based in Los Angeles, California. He is
+            the founder of <Link href="">Allowing Many Forms.</Link>
+          </p>
+          <p className={`indent-16 mt-8`}>Recent</p>
+          <ol className={`list-decimal ml-16`}>
+            <li>
+              Participating in the group exhibition{" "}
+              <span className={`italic`}>LEAF THRU, POUR OVER</span> at{" "}
+              <Link href="https://www.ruthgallery.com/">Ruth Gallery</Link>.
+              November 11 - December 16, 2023.
+            </li>
+            <li>
+              <p>
+                Presented{" "}
+                <span className={`italic`}>
+                  I Know Some Things Form Without You
+                </span>{" "}
+                with{" "}
+                <Link href="https://www.ruthgallery.com/">Ruth Gallery</Link> at{" "}
+                <Link href="https://www.torranceartmuseum.com/tryst">
+                  TRYST
+                </Link>{" "}
+                (Torrance Art Museum). October 27 - 29, 2023.
+              </p>
+            </li>
+            <li>
+              <p>
+                Preparing <Link href="">Red Rotkopf</Link>'s book{" "}
+                <span className={`italic`}>Wind in the Cage</span> for press
+                with <Link href="">Allowing Many Forms</Link>. Forthcoming 2024.
+              </p>
+            </li>
+          </ol>
+
+          {/* TODO: Build the newsletter signup form */}
+          {/* <div className="ml-16">
+            <p className={`mt-8`}>Newsletter</p>
+            <p>Full name</p>
+            <p>Email</p>
+            <p>Submit</p>
+          </div> */}
+
+          <div className="ml-16">
+            <p className={`mt-8`}>Contact</p>
+            <p>
+              <Link href="">taylor@allowingmanyforms.org</Link>
+            </p>
+            <p>Studio in Pasadena, California.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
